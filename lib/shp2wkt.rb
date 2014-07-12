b5256afa7791d001c40eba99035e501d2d57e56e
sagar arlekar <sagar.arlekar@gmail.com>
2014-07-12 20:44:23 +0530

    added module. changed behavior of write.

--- lib/shp2wkt.rb
+++ lib/shp2wkt.rb
@@ -3,46 +3,53 @@
 
 require 'rgeo'
 require 'rgeo/shapefile'
+require 'rgeo/geos'
 
+
+module Shp2Wkt
 # * This class will convert a shapefile to WKT
-class Shp2Wkt
+  class Shp2Wkt
 
-  attr_accessor :filename, :text_geom
-  #
-  # * +filename+ - The path to the shape file. E.g. /home/user/bangalore_roads.shp. The name is stored in @filename
+    attr_accessor :filename
+    #
+    # * +filename+ - The path to the shape file. E.g. /home/user/bangalore_roads.shp. The name is stored in @filename
 
-  def initialize(filename)
-    # absolute path to the shapefile e.g. /home/user/roads.shp
-    @filename = filename
-    read
-  end
+    def initialize(filename)
+      # absolute path to the shapefile e.g. /home/user/roads.shp
+      @filename = filename
+      read
+      write
+    end
 
-  # Will read the shapefile in @filename and convert each geometry in text_geom array as text.
-  def read
-      RGeo::Shapefile::Reader.open(@filename) do |file|
+    # Will read the shapefile in @filename and convert each geometry in text_geom array as text.
+    def read
       @text_geom = []
-      file.each do |record|
-         @text_geom << record.geometry.as_text
+
+      RGeo::Shapefile::Reader.open(@filename) do |file|
+
+        file.each do |record|
+          @text_geom << record.geometry.as_text
+        end
+      end
+      @text_geom
+    end
+
+    # Will write the geometries in text_geom to a file. One geom per line.
+    def write
+
+      outfile_filename = @filename.gsub(".shp", ".txt")
+
+      begin
+        File.open(outfile_filename, 'w') do |op|
+          @text_geom.each do |geom|
+            op.puts "#{geom}"
+          end
+        end
+        @text_geom.length
+      rescue
+        raise Exception
       end
     end
+
   end
-
-  # Will write the geometries in text_geom to a file. One geom per line.
-  # NOTE: Last line will be empty
-  def write()
-
-  outfile_filename = filename.gsub(".shp", ".txt")
-  
-  begin 
-    File.open(outfile_filename, 'w') do |op|  
-      @text_geom.each do |geom|
-          op.write "#{geom}\n" 
-      end
-    end
-    @text_geom.length
-  rescue 
-    raise Exception
-  end
-end
-
 end
